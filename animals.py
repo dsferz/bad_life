940bfe36e02547b3106e1a6977fc7c0e91d3f741
Alexander Svyatov <Alexander_Svyatov@epam.com>
2017-08-25 16:54:50 +0200

    updated classes

--- animals.py
+++ animals.py
@@ -1,34 +1,52 @@
 class predators:
-    meat = True
+    meat_eaters = True
+    gross_eaters = False
     def __init__(self):
         pass
 
 class wolves(predators):
     aggressive_level = 5
+    health = 100
+    life_limit = 100
+    velocity = 1
+    dying_speed = 10
     def __init__(self):
         pass
 
 class wolf_musculine(wolves):
-    def __init__(self):
+    def __init__(self, age = 50):
+        self.age = age
         pass
 
 class wolf_femenine(wolves):
-    def __init__self(self):
+    def __init__(self, age = 50):
+        self.age = age
+        self.pregant = False
         pass
 
 class grosseaters:
-    meat = False
+    meat_eaters = False
+    gross_eaters = True
+    bisex = False
     def __init__(self):
         pass
 
 class rabbits(grosseaters):
+    health = 50
+    velocity = 1
+    life_limit = 100
+    dying_speed = 5
     def __init__(self):
         pass
 
 class rabbit_musculine(rabbits):
-    def __init__(self):
+    def __init__(self, age = 50):
+        self.age = age
+        self.bisex = True
         pass
 
 class rabbit_femenine(rabbits):
-    def __init__(self):
+    def __init__(self, age = 50):
+        self.age = age
+        self.pregant = False
         pass
--- init.py
+++ init.py
@@ -1,3 +1,19 @@
+import animals
+
+
+class A:
+    testval = 100
+    def __init__(self, age = 50):
+        self.age = age
+
+class B(A):
+    testval = 200
+    def __init__(self, value):
+        A()
+        self.testval = value
+
 if __name__ == '__main__':
-    pass
+    test = B(50)
+    test.testval -= 50
+    print test.age
     
