1ca4f3ae470d532e428f2d1e0878b38f16e1aaf8
Alexander Svyatov <Alexander_Svyatov@epam.com>
2017-08-25 16:19:41 +0200

    initial push

---
+++ animals.py
@@ -0,0 +1,34 @@
+class predators:
+    meat = True
+    def __init__(self):
+        pass
+
+class wolves(predators):
+    aggressive_level = 5
+    def __init__(self):
+        pass
+
+class wolf_musculine(wolves):
+    def __init__(self):
+        pass
+
+class wolf_femenine(wolves):
+    def __init__self(self):
+        pass
+
+class grosseaters:
+    meat = False
+    def __init__(self):
+        pass
+
+class rabbits(grosseaters):
+    def __init__(self):
+        pass
+
+class rabbit_musculine(rabbits):
+    def __init__(self):
+        pass
+
+class rabbit_femenine(rabbits):
+    def __init__(self):
+        pass
